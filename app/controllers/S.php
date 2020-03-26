dbf059d559c381076ffd5048690b33f0306770e0
Rudnik <kostyabest@ukr.net>
2020-03-27 01:17:56 +0200

    Some fixes

--- app/controllers/S.php
+++ app/controllers/S.php
@@ -1,31 +1,8 @@
 <?php
 
 class S extends Controller {
-    public function index() {
-        if($_COOKIE['login'] != '') {
-            $data = [];
-
-            $cutter = $this->model('SModel');
-
-            if(isset($_POST['link'])) {
-                $cutter->setData($_POST['link'], $_POST['shortlink']);
-
-                if($cutter->isReserved()) {
-                    $data['message'] = "Короткая ссылка уже занята!";
-                }else{
-                    $cutter->addLink();
-                }
-            }
-
-            if($_POST['delete_item']) {
-                $cutter->deleteLink($_POST['delete_item']);
-            }
-
-            $data['links'] = $cutter->getAllUserLinks();
-
-            $this->view('shortlink/s', $data);
-        }else
-            $this->view('user/reg');
+    public function index($params) {
+        header("Location: http://$params");
     }
 
 
--- app/views/shortlink/s.php
+++ app/views/shortlink/s.php
@@ -18,14 +18,14 @@
     <div class="container main">
         <h1>Сокра.тим</h1>
         <p>Вам нужно сократить ссылку? Сейчас мы это сделаем!</p>
-        <form action="/s" method="post" class="form-control">
+        <form action="/" method="post" class="form-control">
             <input type="text" name="link" placeholder="Длинное название" value=""><br>
             <input type="text" name="shortlink" placeholder="Короткое название" value=""><br>
             <div class="error"><?=$data['message']?></div>
             <button class="btn" id="send">Уменьшить</button>
         </form>
         <div class="links form-control">
-            <?php if ($data['links'] > 0): ?>
+            <?php if (count($data['links']) > 0): ?>
                 <h1>Сокращённые ссылки</h1><br>
             <?endif; ?>
             <?php foreach ($data['links'] as $el): ?>
@@ -33,7 +33,7 @@
                     <div class="links-wrap">
                         <p><b>Длинная:</b> <a href="<?=$el['link']?>"><?=$el['link']?></a></p>
                         <p><b>Короткая:</b> <a href="#">localhost:8888/<?=$el['shortlink']?></a></p>
-                        <form action="/s" method="post">
+                        <form action="/" method="post">
                             <input type="hidden" name="delete_item" value="<?=$el['id']?>">
                             <button class="btn">Удалить <i class="fas fa-trash"></i></button>
                         </form>
